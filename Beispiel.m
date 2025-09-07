158149222137d3f1859657112f82c9569492c3ab
humptybigdump <newvegasenjoyerzzz@gmail.com>
2025-09-07 13:30:05 +0200

    Add files via upload

---
+++ Beispiel.m
@@ -0,0 +1,13 @@
+%% Sample Matlab code
+!mv test.txt test2.txt
+A = [1, 2, 3;... foo
+     4, 5, 6];
+s = 'abcd';
+for k = 1:4
+  Disp(s(k)) % bar
+end
+%{
+create row vector x, then reverse it
+%}
+x = linspace(0,1,101);
+y = x(end:-1:1);
